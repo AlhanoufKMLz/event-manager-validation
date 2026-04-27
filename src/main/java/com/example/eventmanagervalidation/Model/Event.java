2ad80ddcd5c2f4fce3201d46ec7b8ee90c1ffeee
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-04-27 16:23:25 +0300

    fix: bean validation

--- src/main/java/com/example/eventmanagervalidation/Model/Event.java
+++ src/main/java/com/example/eventmanagervalidation/Model/Event.java
@@ -10,21 +10,23 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Event {
     @NotBlank(message = "ID cannot be null")
-    @Size(min = 2, message = "ID should be 2 or more letters.")
+    @Size(min = 2, message = "ID must be at least 2 letters.")
     private String id;
 
     @NotBlank(message = "Description cannot be null")
-    @Size(min = 15, message = "Description should be 15 or more letters.")
+    @Size(min = 15, message = "Description must be at least 15 letters.")
     private String description;
 
-    @NotNull(message = "Capacity cannot be null")
-    @Digits(integer = 26, fraction = 0, message = "Capacity should be an integer greater than 25")
-    @Min(value = 26, message = "Capacity should be more than 25")
+    @Min(value = 26, message = "Capacity must be more than 25")
     private int capacity;
 
 
+    @NotNull(message = "Start date cannot be null")
+    @FutureOrPresent(message = "Start date must be today or in the future")
     private LocalDate startDate;
 
+    @NotNull(message = "End date cannot be null")
+    @Future(message = "End date must be in the future")
     private LocalDate endDate;
 
 }
